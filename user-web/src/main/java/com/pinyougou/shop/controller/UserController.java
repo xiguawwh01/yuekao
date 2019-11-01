3e02c8039958333ff96e266f718c3a2586ffb081
xiguawwh01 <809671829@qq.com>
2019-11-01 15:02:27 +0800

    修改二次

--- user-web/src/main/java/com/pinyougou/shop/controller/UserController.java
+++ user-web/src/main/java/com/pinyougou/shop/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         User uu=sev.login(user.getUname());
 
 
-        System.out.println( "232332333333333");
+        System.out.println( "232332333333");
         return "";
     }
 
